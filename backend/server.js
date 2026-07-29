c8a9a3298d9c31e10ad86aeea077de31c3b5083f
neerajKashyap-cmd <dabarkashyap31@gmail.com>
2026-07-29 13:45:17 +0530

    remove unused seedData utility and its import

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const seedDatabase = require("./utils/seedData");
 
 dotenv.config();
 
